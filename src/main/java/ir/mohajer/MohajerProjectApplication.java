7e1a889b0e0f9f03e042480321f5c09c876cff0d
mahdikarimi422@gmail.com <09388990352@>
2022-03-19 13:32:14 +0330

    create project.

---
+++ src/main/java/ir/mohajer/MohajerProjectApplication.java
@@ -0,0 +1,13 @@
+package ir.mohajer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohajerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MohajerProjectApplication.class, args);
+    }
+
+}
